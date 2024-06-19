21f609e0acb9264c762d3adf06466e98cd0fee84
Muhammad Habibullah <121166723+habibhaseeb@users.noreply.github.com>
2024-06-19 22:37:45 +0500

    Update _01basics.js

--- Custom methods/_01basics.js
+++ Custom methods/_01basics.js
@@ -1,1 +1,9 @@
-//tuples in js
+// within body tag just example we write
+<body>
+ <script type="module">
+   import message from './message.js'
+      // then 
+  let example = message 
+   console.log("example:" + example);
+ </script>
+</body>
