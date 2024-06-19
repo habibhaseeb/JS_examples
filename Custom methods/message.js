4bdd740377724c327b9ffb40980728d547a2fdd3
Muhammad Habibullah <121166723+habibhaseeb@users.noreply.github.com>
2024-06-19 22:41:32 +0500

    Update Custom methods

---
+++ Custom methods/message.js
@@ -0,0 +1,11 @@
+const message = {
+ name: "Habib web developer",
+ age: 19,
+ jobPortal: null,
+ address: "Faisalabad city of Pakistan"
+}
+
+let age = message.age;
+let name = message.name;
+
+export default message;
