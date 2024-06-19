ceb41eca318aec6aabd60fa12d9eaa2b7202bd68
Muhammad Habibullah <121166723+habibhaseeb@users.noreply.github.com>
2024-06-19 22:43:31 +0500

    Update _01basics.js

--- Custom methods/_01basics.js
+++ Custom methods/_01basics.js
@@ -4,6 +4,10 @@
    import message from './message.js'
       // then 
   let example = message 
+
    console.log("example:" + example);
+let print = () => {
+  console.log(name , age) // from file   message.js
+}
  </script>
 </body>
