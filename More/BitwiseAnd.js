54ded8aa9e55401d1588e046f0900a7f03d01a66
Muhammad Habibullah <121166723+habibhaseeb@users.noreply.github.com>
2024-06-24 11:34:10 +0500

    Create BitwiseAnd.js

---
+++ More/BitwiseAnd.js
@@ -0,0 +1,11 @@
+const first = 10;
+const second = 20;
+
+const res = first | second; // this is bitwise and operator 
+console.log(res);
+
+let n1 = 6; // 6 bits
+let n2 = 10; // 10 bits both makes 2 byte
+const res1 = (n1 & n2);
+console.log(res1);
+// there are many diffrent ways for the usage of | Bitwise And | operator.
