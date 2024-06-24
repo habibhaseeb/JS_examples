f9f588bb2b6dc9d9f29462d50078534f1b1cb87e
Muhammad Habibullah <121166723+habibhaseeb@users.noreply.github.com>
2024-06-24 11:32:03 +0500

    Create encode_decode.js

---
+++ More/encode_decode.js
@@ -0,0 +1,11 @@
+const string = "Learning JavaScript";
+
+const result = window.btoa(string);
+console.log(result);
+
+const result1 = window.atob
+                        (string)
+        console.log(result1);
+
+        // this code will always run in inspect element 
+        //  of chrome and other browsers
